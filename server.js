bfbc757f75ca72ad15b0ceb9d6b7a2571e0a0d4f
yonas kemon <yonaskemon01@gmail.com>
2021-06-16 23:38:54 +0100

    uft

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 if (process.env.NODE_ENV === "development") {
-    app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
+    app.use(cors({ origin: process.env.CLIENT_URL }))
 }
 
 app.use('/api', blogRoute)
